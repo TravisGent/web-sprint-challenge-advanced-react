89c236d2abaac198077e8f7eb3123737260275f0
Eorium <travisgent312@gmail.com>
2020-07-10 12:14:49 -0600

    add tests

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -50,7 +50,7 @@ const CheckoutForm = (props) => {
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button data-testid="buttonElement">Checkout</button>
       </form>
 
       {showSuccessMessage && (
